94cbd01ef5fc141bbeb04069d1578d0d6f301d5b
MacBookPro <alteyaa>
2019-09-03 13:05:22 +0600

    extends

--- app/src/main/java/com/myapplication/data/IBoredApiClient.java
+++ app/src/main/java/com/myapplication/data/IBoredApiClient.java
@@ -1,19 +1,18 @@
 package com.myapplication.data;
 
+import com.myapplication.core.ICoreCallback;
 import com.myapplication.model.BoredAction;
 
 public interface IBoredApiClient {
 
     void getBoredAction(
 
-  final BoredActionCallBack callBack
+            final BoredActionCallBack callBack
 
     );
-    interface BoredActionCallBack{
 
+    interface BoredActionCallBack extends ICoreCallback<BoredAction> {
 
-        void onSuccess(BoredAction action);
-        void onFailure(Exception e);
     }
 
 }
