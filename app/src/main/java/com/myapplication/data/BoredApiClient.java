4eec8cd6027556d864ab27df490053c15e98dd1c
MacBookPro <alteyaa>
2019-09-03 13:04:09 +0600

    rewrote retrofit client

--- app/src/main/java/com/myapplication/data/BoredApiClient.java
+++ app/src/main/java/com/myapplication/data/BoredApiClient.java
@@ -1,35 +1,32 @@
 package com.myapplication.data;
 
+import com.myapplication.core.CoreApiClient;
+import com.myapplication.core.ResponseHandler;
 import com.myapplication.model.BoredAction;
 
 import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 
 
-public class BoredApiClient implements IBoredApiClient {
+public class BoredApiClient extends CoreApiClient implements IBoredApiClient {
 
-    private Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("http://www.boredapi.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build();
 
-    BoredNetworkClient client = retrofit.create(BoredNetworkClient.class);
+    BoredNetworkClient client = getRetrofit("http://www.boredapi.com/")
+            .create(BoredNetworkClient.class);
 
     @Override
     public void getBoredAction(BoredActionCallBack callBack) {
 
         Call<BoredAction> call = client.getBoredAction();
 
-        call.enqueue(new CoreCallBack<BoredAction>() {
+        call.enqueue(new ResponseHandler<BoredAction>() {
             @Override
-            void onSuccess(BoredAction result) {
+             public void onSuccess(BoredAction result) {
                 callBack.onSuccess(result);
             }
 
             @Override
-            void onFailure(Exception e) {
+           public void onFailure(Exception e) {
                 callBack.onFailure(e);
 
             }
--- app/src/main/java/com/myapplication/data/ResponseHandler.java
+++
@@ -1,4 +0,0 @@
-package com.myapplication.data;
-
-public class ResponseHandler {
-}
